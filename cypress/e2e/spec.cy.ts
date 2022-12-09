2561d6d1eb42e6854efb1d20e23c3fee8ce1ee61
petraelgemyr <petra.elgemyr@gmail.com>
2022-12-09 15:52:14 +0100

    baseUrl fixed

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,22 @@
-import { IMovie } from "./../../src/ts/models/IMovie";
-import { mockData } from "./../../src/ts/services/__mocks__/movieservice";
+import { IMovie } from "../../src/ts/models/IMovie";
+import { mockData } from "../../src/ts/services/__mocks__/movieservice";
 
-describe("should  getData", () => {
+// beforeEach(()=> {
+//   cy.visit(/)
+// })
+beforeEach(() => {
+  cy.visit("/");
+});
+
+describe("should search movies", () => {
+  it("should run getData and get 10 movies", () => {
+    cy.get("input").type("star").should("have.value", "star");
+    cy.get("form").submit();
+    cy.get("div#movie-container > div.movie").should("have.length", 10);
+  });
+
   it("should get 3 movie divs", () => {
-    cy.visit("http://localhost:1234");
+    // cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
@@ -12,7 +25,7 @@ describe("should  getData", () => {
   });
 
   it("should get 3 headings", () => {
-    cy.visit("http://localhost:1234");
+    // cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
@@ -21,7 +34,7 @@ describe("should  getData", () => {
   });
 
   it("should get 3 img", () => {
-    cy.visit("http://localhost:1234");
+    // cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
@@ -30,7 +43,7 @@ describe("should  getData", () => {
   });
 
   it("should have Harry in title", () => {
-    cy.visit("http://localhost:1234");
+    // cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
@@ -39,7 +52,7 @@ describe("should  getData", () => {
   });
 
   it("should have Star in title", () => {
-    cy.visit("http://localhost:1234");
+    // cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", mockData);
 
     cy.get("form").submit();
@@ -47,22 +60,22 @@ describe("should  getData", () => {
     cy.get("div.movie:first > h3").contains("Star");
   });
 
-  it("should type in input", () => {
-    cy.visit("http://localhost:1234");
+  it("should be able to type in input", () => {
+    // cy.visit("http://localhost:1234");
 
     cy.get("input").type("star").should("have.value", "star");
   });
 
-  it("should have button", () => {
-    cy.visit("http://localhost:1234");
+  it("should have a button", () => {
+    // cy.visit("http://localhost:1234");
 
-    cy.get("button").should("contain", "Sök");
+    cy.get("button").contains("Sök");
   });
 });
 
 describe("displayNoResult", () => {
   it("should show error message", () => {
-    cy.visit("http://localhost:1234");
+    // cy.visit("http://localhost:1234");
     cy.intercept("GET", "http://omdbapi.com/*", { fakeSearch: [] });
 
     cy.get("form").submit();
